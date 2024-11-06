2eed117e889eb982dcf82ecf153b1afb9a69615d
isboyjc <214930661@qq.com>
2024-11-06 11:54:37 +0800

    feat✨: Add editor core extension bubble menu

---
+++ packages/core/src/extensions/bubble-menu.js
@@ -0,0 +1,12 @@
+/*
+ * @LastEditTime: 2024-11-03 22:22:32
+ * @Description: ...
+ * @Date: 2024-11-03 19:47:02
+ * @Author: isboyjc
+ * @LastEditors: isboyjc
+ */
+import BubbleMenu, { BubbleMenuPlugin } from '@tiptap/extension-bubble-menu'
+
+export { BubbleMenuPlugin }
+
+export default BubbleMenu
