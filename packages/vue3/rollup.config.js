e49660a101d31e46d693404ef71f426940231ad6
陈景生 <116934223+LiCHUYA@users.noreply.github.com>
2025-01-06 13:59:19 +0800

    fix: 🐛 Fix build file export and script commands
    * fix:Fixed packaging, style import, file import, project launch issues
    
    * fix:Delete invalid code to ensure normal startup of the project

--- packages/vue3/rollup.config.js
+++ packages/vue3/rollup.config.js
@@ -1,4 +1,7 @@
 import { baseConfig } from "../../shared/rollup/index.js";
 import pkg from "./package.json" assert { type: "json" };
 
-export default baseConfig({ input: "src/index.js", pkg });
+export default baseConfig({
+  input: "src/index.js",
+  pkg,
+});
